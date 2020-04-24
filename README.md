68ce37c362e8faea562684b9c692e1ac23c14bba
marinierb <55813407+marinierb@users.noreply.github.com>
2020-04-24 18:11:47 -0400

    Better handling of secrets

--- README.md
+++ README.md
@@ -6,38 +6,25 @@ It runs each tier in its own docker container.
 
 ## Part 1 - Run it on your local machine
 
-Clone the repo
+1 - Clone the repo
 
     git clone https://github.com/marinierb/DockerChallenge.git
 
-cd to it
+2 - cd to it
 
     cd DockerChallenge
 
-Create a **.env** file with the following variables - Adjust as required
+3 - Adjust the contents of the **.env** file to your preferences - the **non-azure** stuff only at this point
 
-    COMPONENT_NAME_PREFIX=bruno
-    COMPONENT_VERSION=v1.0.0
-    # db
-    POSTGRES_IMAGE=library/postgres:12.2-alpine
-    POSTGRES_HOST=db
-    POSTGRES_USER=docker
-    POSTGRES_PASSWORD=docker
-    POSTGRES_DB=docker
-    POSTGRES_PORT=5432
-    # app
-    NODE_IMAGE=node:13.12.0-alpine3.11
-    APP_HOST=app
-    APP_PORT=3000
-    # web
-    NGINX_IMAGE=nginx:1.17.9-alpine
-    WEB_PORT=80
-    
-Start it
+4 - Create a **.secrets** file with the following variables - set as required
+
+    POSTGRES_PASSWORD=password123    
+
+5 - Start it
 
     docker-compose up -d
 
-Test it
+6 - Test it
 
 URL | Action
 --- | ---
@@ -47,33 +34,23 @@ http://localhost/users | Hits the *users* endpoint which lists the users in the 
 
 ## Part 2 - Deploy it to an Azure Container Group (IaaS)
 
-Add the following to your **.env** file - Adjust as required
+1 - Adjust the contents of the **.env** file to your preferences - the **azure** stuff only at this point
 
-    # azure
-    AZ_RESOURCE_GROUP_NAME=EsSCRaPRg
-    AZ_SKU=basic
-    AZ_LOCATION=canadacentral
-    # IaaS - Container Stuff
-    AZ_CONTAINER_REGISTRY_NAME=${COMPONENT_NAME_PREFIX}Acr
-    AZ_CONTAINER_REGISTRY_SERVER=${AZ_CONTAINER_REGISTRY_NAME}.azurecr.io
-    AZ_CONTAINER_GROUP_NAME=${COMPONENT_NAME_PREFIX}ContainerGroup
-    AZ_CONTAINER_GROUP_DNS=${COMPONENT_NAME_PREFIX}testapp
-
-Log into your Azure account
+2 - Log into your Azure account
 
     az login
 
-Run the deploy script
+3 - Run the deploy script
 
     ./azuredeploy.sh
 
-Test it - Adjust the URL as per the AZ_CONTAINER_GROUP_DNS value
+4 - Test it - adjust the URL as per the AZ_CONTAINER_GROUP_DNS value
 
 URL | Action
 --- | ---
 http://brunotestapp.canadacentral.azurecontainer.io/ | Hits the nginx default page
-http://brunotestapp.canadacentral.azurecontainer.io//app | Hits the *app* endpoint
-http://brunotestapp.canadacentral.azurecontainer.io//users | Hits the *users* endpoint which lists the users in the database
+http://brunotestapp.canadacentral.azurecontainer.io/app | Hits the *app* endpoint
+http://brunotestapp.canadacentral.azurecontainer.io/users | Hits the *users* endpoint which lists the users in the database
 
 
 Note
--- azuredeploy.sh
+++ azuredeploy.sh
@@ -1,33 +1,59 @@
 #!/bin/bash
 
 set -e # exit as soon as a command fails
+set -a # force export on all vars - needed for envsubst
 
-echo "--> Source the environment file"
-# envsubst (below) needs vars to be exported, so...
-set -a   # force export on all vars
+echo "--> Source the environment file..."
 source .env
-set +a   # put things back the way they were
 
-echo "--> Create container registry"
+echo "--> Get database password from key vault..."
+set +e # disable exit on error
+dbPassword=$(az keyvault secret show \
+          --vault-name $AZ_KEYVAULT_NAME \
+          --name $AZ_SECRET_DBPASSWORD_NAME \
+          --query value | sed 's/"//g')
+set -e # re-enable exit on error
+
+if [ "$dbPassword" ]
+then
+  echo " -- Database password found!"
+else
+  echo " -- Database password not found!"
+  echo "--> Create key vault..."
+  az keyvault create \
+    --name $AZ_KEYVAULT_NAME \
+    --resource-group $AZ_RESOURCE_GROUP_NAME \
+    --location $AZ_LOCATION \
+    --output none
+  echo "--> Create a password and put it in the vault..."
+  dbPassword=$(cat /dev/urandom | tr -dc a-zA-Z0-9 | head -c14)
+  az keyvault secret set \
+    --vault-name $AZ_KEYVAULT_NAME \
+    --name $AZ_SECRET_DBPASSWORD_NAME \
+    --value $dbPassword \
+    --output none
+fi
+
+echo "--> Create container registry..."
 az acr create \
   --resource-group $AZ_RESOURCE_GROUP_NAME \
   --name $AZ_CONTAINER_REGISTRY_NAME \
   --sku $AZ_SKU \
   --output none
 
-echo "--> Enable admin on container registry"
+echo "--> Enable admin on container registry..."
 az acr update \
   --name $AZ_CONTAINER_REGISTRY_NAME \
   --admin-enabled true \
   --output none
 
-echo "--> Get container registry password"
+echo "--> Get container registry password..."
 export acrPassword=$(az acr credential show \
                   --name $AZ_CONTAINER_REGISTRY_NAME \
                   --query "passwords[0].value"  \
                   | sed 's/"//g')
 
-echo "--> Push images to container registry"
+echo "--> Push images to container registry..."
 docker login $AZ_CONTAINER_REGISTRY_NAME.azurecr.io --username $AZ_CONTAINER_REGISTRY_NAME --password $acrPassword
 echo " -- Tag images"
 docker tag ${COMPONENT_NAME_PREFIX}_db:$COMPONENT_VERSION  $AZ_CONTAINER_REGISTRY_SERVER/${COMPONENT_NAME_PREFIX}_db:$COMPONENT_VERSION
@@ -45,16 +71,16 @@ docker push $AZ_CONTAINER_REGISTRY_SERVER/${COMPONENT_NAME_PREFIX}_web:$COMPONEN
 #   echo -e "$rep\t$tags"
 # done
 
-echo "--> Build container group yaml file (azuredeploy.yml)"
+echo "--> Build container group yaml file (azuredeploy.yml)..."
 envsubst < azuredeploy.yml.template >azuredeploy.yml
 
-echo "--> Create container group"
+echo "--> Create container group..."
 az container create  \
   --resource-group $AZ_RESOURCE_GROUP_NAME \
   --file azuredeploy.yml \
   --output none
 
-echo "--> Check messages"
+echo "--> Check messages..."
 az container show \
   --resource-group $AZ_RESOURCE_GROUP_NAME \
   --name $AZ_CONTAINER_GROUP_NAME \
